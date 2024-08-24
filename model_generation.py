440bf919ae7236c45f761b94fbf4699d3dfd6b60
Devin Lepur <73671652+devin-lepur@users.noreply.github.com>
2024-08-24 16:11:39 -0700

    Add files via upload
    Removed feature importance filtering

--- model_generation.py
+++ model_generation.py
@@ -13,28 +13,7 @@ from imblearn.over_sampling import SMOTE
 from datetime import datetime
 from sklearn.model_selection import train_test_split
 
-def get_important_features(model, columns):
-    """
-    Return features with importance above a threshold
-    
-    model: An XGBoost model capable of feature_importances_
-    columns (list[str]): columns the model was trained on
-    
-    Returns:
-    list[str] list of column names with unimportant features removed
-    """
-    FEATURE_THRESHOLD = 0.1
-    
-    feature_importance = list(zip(columns, model.feature_importances_))
-    feature_importance.sort(key=lambda x: x[1])
 
-    # If worst feature is below threshold, return all other features
-    if (feature_importance[0][1] < FEATURE_THRESHOLD):
-        important_features = [feature[0] for feature in feature_importance[1:]]
-        return important_features
-    else:
-        # Features deemed important enough, return all    
-        return columns
 
 def create_model(df):
     """
@@ -47,14 +26,14 @@ def create_model(df):
     """
 
     # Threshold for reliable negatives
-    NEG_THRESHOLD = 0.2
+    NEG_THRESHOLD = 0.1
 
     # Separate features and target
     X = df.drop(columns=['is_target'])
     y = df['is_target']
 
     # Init classifier
-    clf = XGBClassifier(eta=0.8, subsample=0.5, reg_alpha=0.1, reg_lambda=0.1)
+    clf = XGBClassifier(eta=0.1, subsample=0.5, reg_alpha=0.1, reg_lambda=0.1)
 
     # Fit classifier, assume 0s initially are negatives
     clf.fit(X, y)
@@ -75,11 +54,11 @@ def create_model(df):
     y_merged = np.concatenate([positive_labels, reliable_negative_labels])
 
     # Oversample minority class using SMOTE
-    smote = SMOTE(sampling_strategy='minority')
-    X_over, y_over = smote.fit_resample(X_merged, y_merged)
+    #smote = SMOTE(sampling_strategy='minority')
+    #X_over, y_over = smote.fit_resample(X_merged, y_merged)
 
     # Retrain classifier
-    clf.fit(X_over, y_over)
+    clf.fit(X_merged, y_merged)
 
     return clf
 
@@ -101,16 +80,5 @@ def get_user_model(df):
 
     # Rename for loop purposes
     filtered_df = df
-    
-    # Loop until all features are deemed important
-    while True:
-        current_features = filtered_df.drop(columns=['is_target']).columns
-        new_features = get_important_features(model, current_features)
-        if len(current_features) == len(new_features):
-            break
-
-        # Ensure is_target column is not lost
-        filtered_df = filtered_df.loc[:, new_features + ['is_target']]
-        model = create_model(filtered_df)
 
     return model, filtered_df.columns
